613ef6885d2adebda4fc2c47bf19faffbcc71f37
Tiago Ribeiro Carneiro <tiago.rib@gmail.com>
2023-08-26 15:38:17 -0300

    feat: implements crud customer complete

--- src/main/java/br/com/fatec/FatecProjectRestAPI/controller/CustomerController.java
+++ src/main/java/br/com/fatec/FatecProjectRestAPI/controller/CustomerController.java
@@ -7,9 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api/v1/customer")
@@ -21,14 +22,31 @@ public class CustomerController {
     @GetMapping(value = "/list")
     public ResponseEntity<Object> list() {
         List<Customer> result = customerService.getInfoCustomers();
-        return ResponseEntity.ok().body(ResponseGenericException.response(result));
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseGenericException.response(result));
     }
 
     @PostMapping(value = "/create")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Customer saveCustomer(@RequestBody Customer customer) {
-        return customerService.saveCustomer(customer);
+    public ResponseEntity<Object> saveCustomer(@RequestBody Customer customer) {
+        Customer result = customerService.saveCustomer(customer);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseGenericException.response(result));
     }
 
+    @DeleteMapping(value = "/delete/{idCustomer}")
+    public ResponseEntity<Object> deleteCustomer(@PathVariable Long idCustomer) {
+        HashMap<String, Object> result = customerService.deleteCustomer(idCustomer);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseGenericException.response(result));
+    }
+
+    @GetMapping(value = "/findCustomer/{idCustomer}")
+    public ResponseEntity<Object> getCustomerById(@PathVariable Long idCustomer){
+        Optional<Customer> result = customerService.findCustomerById(idCustomer);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseGenericException.response(result));
+    }
+
+    @PutMapping(value = "/update")
+    public ResponseEntity<Object> updateCustomer(@RequestBody Customer customer) {
+        Customer result = customerService.updateCustomer(customer);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseGenericException.response(result));
+    }
 
 }
